5a012706849e6f8b30f288a9af4de31e232f7637
NIIKHIL KUMAR <131521393+Nikhil-kumar2004@users.noreply.github.com>
2024-06-28 17:33:48 +0000

    conversions

---
+++ javascript/03_conversions.js
@@ -0,0 +1,27 @@
+let score=undefined
+let scoreno=Number(score)
+console.log(typeof(scoreno)) //number
+console.log(scoreno) //33
+
+/*
+all the below will get converted and type will be number but while printing result will be 
+different.
+"33abc"-->NaN(not a number)
+null-->0
+undefined-->NaN
+true-->1, false-->0
+*/
+
+let value=3
+let ans=Boolean(value)
+console.log(ans)
+/*
+1..N-->true, 0-->false
+""-->false, "nikhil"-->true
+null,undefined-->false
+*/
+let avg=undefined
+let k=String(avg)
+console.log(typeof k) //string
+console.log(k)// null / undefined
+//string conversion doesn't have exception.
