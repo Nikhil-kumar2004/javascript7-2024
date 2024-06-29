80c4e61ac10fe67aca142cdf45b9b88d2c1c8388
NIIKHIL KUMAR <131521393+Nikhil-kumar2004@users.noreply.github.com>
2024-06-29 18:37:59 +0000

    strings operations

---
+++ javascript/06_strings.js
@@ -0,0 +1,24 @@
+let name="  nikhil kumar  "
+let age="20"
+console.log("hi " + name + " you are "+age) //old way.
+console.log(`Hi ${name} you are ${age}`) //more readable //modern way 
+// called string interpolation
+const str=new String("nikk") //using objects of javascript.
+console.log(name.length)
+console.log(name.toUpperCase()) //string has various methods.
+console.log(name.charAt(2))
+console.log(name.indexOf('i'))
+
+let str2=name.substring(0,4) // here -ve index is ignored and from 0 it starts.
+let str3=name.slice(-4,4) // '-' indicates indexing starts from last.
+console.table({str2,str3})
+let str4=name.split(' ') //gives array(object).
+let words=["hi","you"]
+let str5=words.join('.')
+console.log(str5) //hi.you
+
+console.log(name.trim()) //trims starting and ending spaces //trimStart(),trimEnd()
+
+console.log(name.replace(' ','-')) //first occurance only.
+console.log(name.replaceAll(' ','-'))
+console.log(name.includes('nik')) //returns true if the substring is there in string.
